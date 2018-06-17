48b625eecf3112aad7221eb5c6c103c4bf022373
slyli <slylicn@qq.com>
2018-06-16 22:25:14 -0700

    begin

---
+++ test/bar.c
@@ -0,0 +1,11 @@
+void myprint(char *msg,int len);
+
+int choose(int a,int b)
+{
+    if(a>b)
+        myprint("the first one\n",15);
+    else
+        myprint("the second one\n",16);
+
+    return 0;
+}
